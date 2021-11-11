20dd23d589b15e5e812449940adf7d1186aae55c
Tianqing <tz2372@gmail.com>
2021-11-11 16:51:22 -0500

    add tests

--- tests/test_cipher_tz2372.py
+++ tests/test_cipher_tz2372.py
@@ -1,1 +1,31 @@
-from cipher_tz2372 import cipher_tz2372
+from cipher_tz2372 import cipher_tz2372 as cp
+
+import pytest
+
+
+# test for single word "mds"
+def test_cipher_positive_shift():
+    example = 'mds'
+    expected = 'net'
+    actual = cp.cipher(example, 1, encrypt=True)
+    assert expected==actual, "they are not equal!"
+
+
+# test for single word "mds" + negative shift
+def test_cipher_negative_shift():
+    example = 'mds'
+    expected = 'lcr'
+    actual = cp.cipher(example, -1, encrypt=True)
+    assert expected==actual, "they are not equal!"
+
+
+def test_cipher_not_alphabet():
+    example = 'mds@qmss'
+    expected = 'net@rntt'
+    actual = cp.cipher(example, 1, encrypt=True)
+    assert expected==actual, "they are not equal!"
+
+
+def test_cipher_exception():
+    with pytest.raises(Exception):
+        cp.cipher("mds", "two", encrypt=True)
